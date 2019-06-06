ac405737876d9370a2ef0875775522c87cad95df
ilyashishlachev <ilya.shishlachev@techs.com.ua>
2019-06-06 14:49:42 +0300

    fix task 1

--- javascript/module-03/task-1.js
+++ javascript/module-03/task-1.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const logins = ['Mango', 'robotGoogles', 'Poly', 'Aj4x1sBozz', 'qwerty123'];
 
 /**
@@ -6,12 +8,7 @@ const logins = ['Mango', 'robotGoogles', 'Poly', 'Aj4x1sBozz', 'qwerty123'];
  * @returns {boolean}
  */
 const isLoginValid = function (login) {
-  if (login.length >= 4 && login.length <= 16) {
-    return true;
-  } else {
-    console.log('Mistake! Login must be from 4 to 16 characters');
-    return false;
-  }
+  return login.length >= 4 && login.length <= 16;
 };
 
 /**
@@ -21,12 +18,15 @@ const isLoginValid = function (login) {
  * @returns {boolean}
  */
 const isLoginUnique = function (all, login) {
-  if (!all.includes(login)) {
-    return true;
-  } else {
-    console.log('This login is already in use!');
-    return false;
-  }
+  return !all.includes(login);
+};
+
+/**
+ * Displays the status of adding a login to the console
+ * @param {string} message
+ */
+const printMessage = function (message) {
+  return console.log(message);
 };
 
 /**
@@ -35,11 +35,22 @@ const isLoginUnique = function (all, login) {
  * @param {string} login
  */
 const addLogin = function (all, login) {
+  if (!(isLoginValid(login))) {
+    printMessage('Mistake! Login must be from 4 to 16 characters');
+  } else {
+    isLoginUnique(all, login);
+  }
+
+  if (!(isLoginUnique(all, login))) {
+    printMessage('This login is already in use!');
+  }
+
   if (isLoginValid(login) && isLoginUnique(all, login)) {
     all.push(login);
-    console.log('Login successfully added!');
+    printMessage('Login successfully added!');
   }
 };
+
 
 addLogin(logins, 'Ajax');
 addLogin(logins, 'robotGoogles');
